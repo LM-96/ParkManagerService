a7dc1f17609bdb7cc3c59780a3f6bbc7bfcb96fd
LM-96 <luca.marchegiani.96@gmail.com>
2021-06-26 19:32:21 +0200

    Requirement analysis "terminated". Started problem analysis

---
+++ Eclipse/ParkManagerService/carparking.pl
@@ -0,0 +1,5 @@
+%====================================================================================
+% carparking description   
+%====================================================================================
+context(ctxcarparking, "localhost",  "TCP", "8000").
+ qactor( parkingservicehui, ctxcarparking, "it.unibo.parkingservicehui.Parkingservicehui").
