f6d83b6b24ed8b2071fe332ca03a64073d4dc1b6
LM-96 <luca.marchegiani.96@gmail.com>
2021-07-29 09:05:26 +0200

    Minor bug fixed

--- it.unibo.parkmanagerservice/src/it/unibo/parkmanagerservice/controller/ControllerBuilder.kt
+++ it.unibo.parkmanagerservice/src/it/unibo/parkmanagerservice/controller/ControllerBuilder.kt
@@ -4,7 +4,6 @@ import it.unibo.parkmanagerservice.bean.DoorsManager
 import it.unibo.parkmanagerservice.persistence.DoorQueue
 import it.unibo.parkmanagerservice.persistence.ParkingSlotRepository
 import it.unibo.parkmanagerservice.persistence.UserRepository
-import sun.plugin.dom.exception.InvalidStateException
 
 object ControllerBuilder {
 
@@ -12,7 +11,7 @@ object ControllerBuilder {
 
     @JvmStatic fun get() : ParkManagerServiceController {
         if(SINGLETON == null) {
-            throw InvalidStateException("ParkManagerServiceControllers | Call method create or inject before")
+            throw IllegalStateException("ParkManagerServiceControllers | Call method create or inject before")
             System.exit(-1)
         }
         return SINGLETON!!
