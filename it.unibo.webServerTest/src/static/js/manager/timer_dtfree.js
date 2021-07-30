64063e33c16fc6cb6102fdc3428a11d06b8574aa
Simone Mattioli <simone.mattioli6@studio.unibo.it>
2021-07-30 18:47:59 +0100

    fix minor bug

--- it.unibo.webServerTest/src/static/js/manager/timer_dtfree.js
+++ it.unibo.webServerTest/src/static/js/manager/timer_dtfree.js
@@ -1,12 +1,11 @@
-const thermometer_socket = new WebSocket(
+const timerdtfree_socket = new WebSocket(
     'ws://' +
     window.location.host +
     '/manager/timerdtfree/'
 );
 
-thermometer_socket.onmessage = function (e) {
+timerdtfree_socket.onmessage = function (e) {
     const data = JSON.parse(e.data);
     console.log(data)
-    status_thermometer.innerHTML = data;
 }
 
