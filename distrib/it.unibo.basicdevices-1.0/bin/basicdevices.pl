eba13735c169a6523c95bbfcc529bdbd4c9b5b17
LM-96 <luca.marchegiani.96@gmail.com>
2021-08-08 14:37:43 +0200

    Updated distib

--- distrib/it.unibo.basicdevices-1.0/bin/basicdevices.pl
+++ distrib/it.unibo.basicdevices-1.0/bin/basicdevices.pl
@@ -5,7 +5,7 @@ context(ctxthermometer, "localhost",  "TCP", "8001").
 context(ctxfan, "localhost",  "TCP", "8002").
 context(ctxsonar, "localhost",  "TCP", "8003").
 context(ctxweightsensor, "localhost",  "TCP", "8004").
-context(ctxcarparking, "192.168.1.100",  "TCP", "8010").
+context(ctxcarparking, "127.0.0.1",  "TCP", "8010").
  qactor( thermometeractor, ctxthermometer, "it.unibo.thermometeractor.Thermometeractor").
   qactor( fanactor, ctxfan, "it.unibo.fanactor.Fanactor").
   qactor( sonaractor, ctxsonar, "it.unibo.sonaractor.Sonaractor").
--- distrib/it.unibo.parkmanagerservice-1.0/bin/carparking.pl
+++ distrib/it.unibo.parkmanagerservice-1.0/bin/carparking.pl
@@ -2,17 +2,18 @@
 % carparking description   
 %====================================================================================
 context(ctxcarparking, "localhost",  "TCP", "8010").
-context(ctxthermometer, "192.168.1.100",  "TCP", "8001").
-context(ctxfan, "192.168.1.100",  "TCP", "8002").
-context(ctxsonar, "192.168.1.100",  "TCP", "8003").
-context(ctxweightsensor, "192.168.1.100",  "TCP", "8004").
+context(ctxthermometer, "127.0.0.1",  "TCP", "8001").
+context(ctxfan, "127.0.0.1",  "TCP", "8002").
+context(ctxsonar, "127.0.0.1",  "TCP", "8003").
+context(ctxweightsensor, "127.0.0.1",  "TCP", "8004").
+context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
  qactor( fanactor, ctxfan, "external").
   qactor( weightsensoractor, ctxweightsensor, "external").
   qactor( sonaractor, ctxsonar, "external").
+  qactor( basicrobot, ctxbasicrobot, "external").
   qactor( parkingmanagerservice, ctxcarparking, "it.unibo.parkingmanagerservice.Parkingmanagerservice").
   qactor( itocccounter, ctxcarparking, "it.unibo.itocccounter.Itocccounter").
   qactor( dtfreecounter, ctxcarparking, "it.unibo.dtfreecounter.Dtfreecounter").
-  qactor( parkingservicestatusgui, ctxcarparking, "it.unibo.parkingservicestatusgui.Parkingservicestatusgui").
   qactor( notificationactor, ctxcarparking, "it.unibo.notificationactor.Notificationactor").
   qactor( antifireactor, ctxcarparking, "it.unibo.antifireactor.Antifireactor").
   qactor( trolley, ctxcarparking, "it.unibo.trolley.Trolley").
